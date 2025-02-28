b4a02566f3869bdc3d8a6387287c1249bc9e105f
abdullah <abdhazouritr@gmail.com>
2025-03-01 02:50:44 +0300

    a

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ["pdfjs-dist"],
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
