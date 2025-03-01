784bb621767d5963d73391cac7511ba1a6be9db1
abdullah <abdhazouritr@gmail.com>
2025-03-01 03:19:41 +0300

    g

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import ContextProvider from "./context/Context.jsx";
+import ContextProvider from "../context/Context.jsx";
 
 createRoot(document.getElementById("root")).render(
   <ContextProvider>
